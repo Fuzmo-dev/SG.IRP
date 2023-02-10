22bc12db13f443fc3a793e242857d8c6d948931a
Pyr0 <121192159+Pyr0dv@users.noreply.github.com>
2023-02-10 21:22:15 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', () => {
+    const toast = new bootstrap.Toast(toastLiveExample)
+
+    toast.show()
+  })
+}
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
+
+
+
+var   button = document.querySelector('.js');
+console.log(button);
+var   h1 = document.querySelector('.h1');
+
+button.onclick = function (){
+  h1.innerHTML = '<img src="a_a94dc4074a66bd808a30cb369c23204e.gif"></img>';
+}
